bab62d74c76b9e1825e632d445796e9cfb478694
tycebrown <91034018+tycebrown@users.noreply.github.com>
2024-11-22 18:40:00 -0600

    basic setup working locally

---
+++ yet-another-stopwatch/src/App.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import {ref} from 'vue';
+
+const msg = ref(undefined);
+
+function loadMsg(){
+  return fetch('http://localhost:5000/api/msg').then(async (data) => msg.value = await data.text());
+}
+
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld v-if="msg" :msg />
+      <div v-else>No message received</div>
+      <button @click="loadMsg()">
+        Click me please!
+      </button>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
